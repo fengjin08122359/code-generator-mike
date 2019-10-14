967525a11717bda0040efa57bf3880308440e960
mike <fj110808@163.com>
2019-10-14 09:47:41 +0800

    fix: .

---
+++ server/template/projectsrc/basicComponents/eltable/Eltable.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-table class="eltable"
+    :data="rawData.table.data"
+    stripe
+    style="width: 100%">
+    <el-table-column v-for="(item, index) in rawData.table.col" :key="index" :prop="item.prop" :label="item.label" :width='item.width'>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+import handle, { eltable } from "./index";
+export default {
+  name: 'Eltable',
+  data() {
+    return {
+      eltable: eltable,
+      raw: {
+        table: {
+          data: [{
+            date: '2016-05-02',
+            name: '王小虎'
+          }],
+          col:[{
+            prop: 'date',
+            label: '日期',
+            width: 120
+          },{
+            prop: 'name',
+            label: '姓名',
+            width: 120
+          }],
+          type: 'table'
+        }
+      }
+    }
+  },
+  props: ['rawData'],
+  created() {
+    handle.created()
+  },
+  mounted() {
+    handle.mounted()
+  },
+}
+</script>
+
+<style>
+
+</style>
--- server/template/projectsrc/basicComponents/index.js
+++ server/template/projectsrc/basicComponents/index.js
@@ -1,25 +1,93 @@
+import {Handle, DataHandle, rgHandle} from 'nclient-microfront';
 import Vue from 'vue'
 import Elbutton from './elbutton/Elbutton'
-import Elcheckbox from './elcheckbox/Elcheckbox'
-import EldateTimePicker from './eldateTimePicker/EldateTimePicker'
 import Elicon from './elicon/Elicon'
-import Elimage from './elimage/Elimage'
-import Elinput from './elinput/Elinput'
 import Elradio from './elradio/Elradio'
+import Elcheckbox from './elcheckbox/Elcheckbox'
+import Elinput from './elinput/Elinput'
+import EldateTimePicker from './eldateTimePicker/EldateTimePicker'
+import Elimage from './elimage/Elimage'
 import Elselect from './elselect/Elselect'
 import Elswitch from './elswitch/Elswitch'
-import Ndiv from './ndiv/Ndiv'
-import Nlabel from './nlabel/Nlabel'
+import Eltable from './eltable/Eltable'
+function upperCamelCase(str){ 
+  var reg = /\b(\w)|\s(\w)/g; 
+  return str.replace(reg,function(m){return m.toUpperCase()}) 
+}
 
-Vue.component(Elbutton.name, Elbutton)
-Vue.component(Elcheckbox.name, Elcheckbox)
-Vue.component(EldateTimePicker.name, EldateTimePicker)
-Vue.component(Elicon.name, Elicon)
-Vue.component(Elimage.name, Elimage)
-Vue.component(Elinput.name, Elinput)
-Vue.component(Elradio.name, Elradio)
-Vue.component(Elselect.name, Elselect)
-Vue.component(Elswitch.name, Elswitch)
-Vue.component(Ndiv.name, Ndiv)
-Vue.component(Nlabel.name, Nlabel)
+class Ele extends DataHandle{
+  constructor() {
+    super('eleComp')
+    this.list = [{
+      type: 'elbutton', 
+      name: 'el-button',
+      comps: Elbutton
+    },{
+      type: 'elicon',
+      name: 'el-icon',
+      comps: Elicon
+    },{
+      type: 'elradio',
+      name: 'el-radio',
+      comps: Elradio
+    },{
+      type: 'elcheckbox',
+      name: 'el-checkbox',
+      comps: Elcheckbox
+    },{
+      type: 'elinput',
+      name: 'el-input',
+      comps: Elinput
+    },{
+      type: 'eldateTimePicker',
+      name: 'el-dateTimePicker',
+      comps: EldateTimePicker
+    },{
+      type: 'elimage',
+      name: 'el-image',
+      comps: Elimage
+    },{
+      type: 'elselect',
+      name: 'el-select',
+      comps: Elselect
+    },{
+      type: 'elswitch',
+      name: 'el-switch',
+      comps: Elswitch
+    },{
+      type: 'eltable',
+      name: 'el-table',
+      comps: Eltable
+    }]
+    this.comps = this.list.reduce((total,item) => {
+      total[upperCamelCase(item.type)] = item.comps
+      return total
+    },{})
+    this.list.forEach(item => {
+      Vue.component(item.comps.name,item.comps)
+      rgHandle.compList.add(item.type, item.comps.data().raw || {}, item.comps.data().style || {})
+    })
+  }
+  init () {
+    console.log('Ele init')
+  }
+}
 
+let ele = new Ele()
+
+let handle = new Handle({
+  name: 'eleComp',
+  created () {
+    ele.init()
+    console.log('ele created')
+  },
+  mounted () {
+    console.log('ele mounted')
+  },
+})
+
+export default handle
+
+export {
+  ele
+}
